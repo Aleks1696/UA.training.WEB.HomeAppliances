07dc1354b5d817ec006df198059a5b9f2a9daf42
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-11 14:07:38 +0200

    Constants returning resource bundle keys

---
+++ src/main/java/com/view/Messages.java
@@ -0,0 +1,16 @@
+package com.view;
+
+public interface Messages {
+    String DIGITAL_WATCH_PLUG_IN = "device.digitalWatch.plugIn.message";
+    String DIGITAL_WATCH_PLUG_OFF = "device.digitalWatch.plugOff.message";
+    String REFRIGERATOR_PLUG_IN = "device.refrigerator.plugIn.message";
+    String REFRIGERATOR_PLUG_OFF = "device.refrigerator.plugOff.message";
+    String SMARTPHONE_PLUG_IN = "device.smartPhone.plugIn.message";
+    String SMARTPHONE_PLUG_OFF = "device.smartPhone.plugOff.message";
+    String TV_PLUG_IN = "device.TV.plugIn.message";
+    String TV_PLUG_OFF = "device.TV.plugOff.message";
+    String ALREADY_PLUGGED = "service.device.isAlreadyPlugged.message";
+    String NOT_PLUGGED = "service.device.isNotPlugged.message";
+    String SORT_BY_POWER = "service.success.sortingByPowerUsage.message";
+    String APARTMENT_IS_CREATED = "service.apartmentIsCreated.message";
+}
