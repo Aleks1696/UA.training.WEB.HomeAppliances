195caa773fc2a6dcc5c00b8414292187f5b5bbf9
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-11 12:07:34 +0200

    servlet sets request attribute and returns it back to web

--- src/main/java/com/controller/servlet/ApartmentServlet.java
+++ src/main/java/com/controller/servlet/ApartmentServlet.java
@@ -12,37 +12,7 @@ public class ApartmentServlet extends HttpServlet {
     private final String jsp = "/WEB-INF/view/apartment.jsp";
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestedURI = req.getServletPath();
-        Controller controller = (Controller)req.getSession().getAttribute("controller");
-        switch (requestedURI) {
-            case "/createApartment": {
-                controller = new Controller();
-                req.getSession().setAttribute("controller", controller);
-                controller.createApartment();
-                req.getSession().setAttribute("devices", controller.getDevicesList());
-                break;
-            }
-            case "/plugIn": {
-                req.setAttribute("response", controller.plugInDevice());
-                break;
-            }
-            case "/plugOff": {
-                req.setAttribute("response", controller.plugOffDevice());
-                break;
-            }
-            case "/getPowerUsage": {
-                req.setAttribute("response", controller.getPowerUsage());
-                break;
-            }
-            case "/sortByPowerUsage": {
-                controller.sortByPowerUsage();
-                break;
-            }
-            case "/findDevice": {
-                req.setAttribute("response", controller.findDevice());
-                break;
-            }
-        }
+        req.setAttribute("response", Controller.getInstance().process(req));
         req.getRequestDispatcher(jsp).forward(req, resp);
     }
 }
