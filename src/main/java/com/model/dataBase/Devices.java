10a5c5a69d3b3e130fbcc5b240cd3d5fa75f7626
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-11 12:13:08 +0200

    Device constructor was changed as a result all devices implemnted those changes

--- src/main/java/com/model/dataBase/Devices.java
+++ src/main/java/com/model/dataBase/Devices.java
@@ -1,20 +1,16 @@
 package com.model.dataBase;
 
-import com.model.entity.Device;
-import com.model.entity.device.DigitalWatch;
-import com.model.entity.device.Refrigerator;
-import com.model.entity.device.SmartPhone;
-import com.model.entity.device.TV;
+import com.model.entity.*;
 import com.model.types.Brand;
 import com.model.types.Resolution;
 import java.util.ArrayList;
 import java.util.List;
 
 public enum Devices {
-    SMARTPHONE(new SmartPhone(Brand.XIAOMI, 5.4, 6, 128, true, 80)),
-    DIGITALWATCH(new DigitalWatch(Brand.SUUNTO, "Green", true, 15)),
-    REFRIGIRATOR(new Refrigerator(Brand.BOSCH, 2.2, 214, "Grey", 250)),
-    TV(new TV(Brand.KIVI, 49.9, Resolution.QUAD_HD, true, 120));
+    SMARTPHONE(new SmartPhone(Brand.XIAOMI, 80, 5.4, 128, 8, true)),
+    DIGITALWATCH(new DigitalWatch(Brand.SUUNTO, 15, "Green", true)),
+    REFRIGERATOR(new Refrigerator(Brand.BOSCH, 250, 214, 200, "Grey")),
+    TV(new TV(Brand.KIVI, 120, 49.9, Resolution.QUAD_HD, true));
 
     private Device device;
 
@@ -32,6 +28,5 @@ public enum Devices {
 
     public static Device getDevice(Devices devices) {
         return devices.device;
-
     }
 }
